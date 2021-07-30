ec0dac4e5dd83c1bc2052312125d37da8fa4d9f4
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-07-30 17:39:00 +0300

    Add files via upload

---
+++ FloristSolution/ApplicationCore/Entity/Audit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entity
+{
+    public abstract class Audit
+    {
+        /// <summary>
+        /// Veri tabanında veri oluşturma işlemi yapan kişinin id'si
+        /// </summary>
+    public int CreatedUserId { get; set; }
+
+    /// <summary>
+    /// Veri tabanında veri oluşturulma tarihi
+    /// </summary>
+    public DateTime CreatedDate { get; set; }
+
+    /// <summary>
+    /// Veri tabanında veri güncelleyen kişi id'si
+    /// </summary>
+    public int? ModifiedUserId { get; set; }
+
+    /// <summary>
+    /// Veri tabanında veri güncelleme tarihi
+    /// </summary>
+    public DateTime? ModifiedDate { get; set; }
+
+    //? işareti değeri null olabilir demek.
+
+    }
+
+}
+
