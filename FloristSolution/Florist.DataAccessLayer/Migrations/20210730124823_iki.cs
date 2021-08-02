11b5b3efacee8f866877d734fde835aefc56d370
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-08-02 13:45:12 +0300

    Add files via upload

---
+++ FloristSolution/Florist.DataAccessLayer/Migrations/20210730124823_iki.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Florist.DataAccessLayer.Migrations
+{
+    public partial class iki : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
