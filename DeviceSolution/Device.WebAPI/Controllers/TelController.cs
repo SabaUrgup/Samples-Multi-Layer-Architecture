1cab191acfd82fa49b6f9b9bc4844935e269e11d
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-08-03 09:52:41 +0300

    Swagger & controllers part

---
+++ DeviceSolution/Device.WebAPI/Controllers/TelController.cs
@@ -0,0 +1,70 @@
+﻿using Device.Business.Abstract;
+using Device.DataAccessLayer.DataTransferObject.TelDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Device.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelController : ControllerBase
+    {
+        public ITelService _telService;
+        public TelController(ITelService telService)
+        {
+            _telService = telService;
+        }
+
+        [HttpGet]
+        [Route("TelList")]
+        public ActionResult<List<TelListDTO>> ListTel()
+        {
+            var telList = _telService.ListTel();
+            if (telList == null)
+            {
+                return BadRequest("Telefon listesi bulunamadı!");
+            }
+            return Ok(telList);
+        }
+
+        [HttpDelete]
+        [Route("TelDelete")]
+        public ActionResult<string> DeleteTel(int Id)
+        {
+            var result = _telService.DeleteTel(Id);
+            if (result > 0)
+            {
+                return Ok("Silinme başarılı!");
+            }
+            return BadRequest("Silinme başarısız!");
+        }
+
+        [HttpPost]
+        [Route("TelAdd")]
+        public ActionResult<string> AddTel(TelAddDTO tel)
+        {
+            var result = _telService.AddTel(tel);
+            if (result > 0)
+            {
+                return Ok("Ekleme Başarılı!");
+            }
+            return BadRequest("Ekleme Başarısız!");
+        }
+
+        [HttpPut]
+        [Route("TelUpdate")]
+        public ActionResult<string> UpdateTel(TelUpdateDTO tel)
+        {
+            var result = _telService.UpdateTel(tel);
+            if (result > 0)
+            {
+                return Ok("Güncelleme başarılı!");
+            }
+            return BadRequest("Güncelleme Başarısız!");
+        }
+    }
+}
---
+++ DeviceSolution/Device.WebAPI/Controllers/TvController.cs
@@ -0,0 +1,70 @@
+﻿using Device.Business.Abstract;
+using Device.DataAccessLayer.DataTransferObject.TvDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Device.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TvController : ControllerBase
+    {
+        public ITvService _tvService;
+        public TvController(ITvService tvService)
+        {
+            _tvService = tvService;
+        }
+
+        [HttpGet]
+        [Route("TvList")]
+        public ActionResult<List<TvListDTO>> ListTv()
+        {
+            var tvList = _tvService.ListTv();
+            if (tvList == null)
+            {
+                return BadRequest("Televizyon listesi bulunamadı!");
+            }
+            return Ok(tvList);
+        }
+
+        [HttpDelete]
+        [Route("TvDelete")]
+        public ActionResult<string> DeleteTv(int Id)
+        {
+            var result = _tvService.DeleteTv(Id);
+            if (result > 0)
+            {
+                return Ok("Silinme başarılı!");
+            }
+            return BadRequest("Silinme başarısız!");
+        }
+
+        [HttpPost]
+        [Route("TvAdd")]
+        public ActionResult<string> AddTel(TvAddDTO tv)
+        {
+            var result = _tvService.AddTv(tv);
+            if (result > 0)
+            {
+                return Ok("Ekleme Başarılı!");
+            }
+            return BadRequest("Ekleme Başarısız!");
+        }
+
+        [HttpPut]
+        [Route("TvUpdate")]
+        public ActionResult<string> UpdateTv(TvUpdateDTO tv)
+        {
+            var result = _tvService.UpdateTv(tv);
+            if (result > 0)
+            {
+                return Ok("Güncelleme başarılı!");
+            }
+            return BadRequest("Güncelleme Başarısız!");
+        }
+    }
+}
