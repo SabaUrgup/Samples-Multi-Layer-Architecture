b3131b5cfed8b26b7eccfd6ab9acb01d20896b8c
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-08-03 09:40:43 +0300

    Layer where common entities are kept

---
+++ DeviceSolution/AplicationCore/Entity/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicationCore.Entity
+{
+    /// <summary>
+    /// Varlıkların ortak ana özellikleri tutan interface
+    /// </summary>
+    public interface IEntity
+    {
+    }
+}
---
+++ DeviceSolution/AplicationCore/Entity/ISoftDelete.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicationCore.Entity
+{
+    /// <summary>
+    /// Veri tabanında kontrollü silme işlemi yapılan yer.
+    /// </summary>
+    public interface ISoftDelete
+    {
+        public bool IsDeleted { get; set; }
+    }
+}
