85e45f6eb48ff1a9d833c99af8fb9fb27829cd3e
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-08-03 09:54:52 +0300

    Swagger & controllers part

---
+++ DeviceSolution/Device.WebAPI/Startup.cs
@@ -0,0 +1,66 @@
+using Device.Business.Abstract;
+using Device.Business.Concrete;
+using Device.DataAccessLayer.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Device.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Device.WebAPI", Version = "v1" });
+            });
+
+            services.AddDbContext<DeviceDatabaseContext>();
+            // her nesne için bir defa oluþturulur --> AddDbContext
+
+            services.AddScoped<ITvService, TvService>();
+            services.AddScoped<ITelService, TelService>();
+            // Ýstek geldikçe nesne oluþturur --> AddScoped
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Device.WebAPI v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
