edd1e4b4c8b41c1b98ee360ae5082b19dff268b5
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-08-03 09:46:03 +0300

    CRUD operations layer

---
+++ DeviceSolution/Device.Business/Abstract/ITelService.cs
@@ -0,0 +1,21 @@
+﻿using Device.DataAccessLayer.DataTransferObject.TelDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Device.Business.Abstract
+{
+    public interface ITelService
+    {
+        public int AddTel(TelAddDTO tel);
+
+        public int UpdateTel(TelUpdateDTO tel);
+
+        public int DeleteTel(int Id);
+
+        public List<TelListDTO> ListTel();
+
+    }
+}
