d97bcd72d3c9febb61d57bd049b872f44e667f51
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-08-03 14:29:07 +0300

    Update Startup.cs

--- DeviceSolution/Device.WebAPI/Startup.cs
+++ DeviceSolution/Device.WebAPI/Startup.cs
@@ -36,11 +36,28 @@ namespace Device.WebAPI
             });
 
             services.AddDbContext<DeviceDatabaseContext>();
-            // her nesne için bir defa oluþturulur --> AddDbContext
+            // her nesne iÃ§in bir defa oluÅŸturulur --> AddDbContext
 
             services.AddScoped<ITvService, TvService>();
             services.AddScoped<ITelService, TelService>();
-            // Ýstek geldikçe nesne oluþturur --> AddScoped
+            // Ä°stek geldikÃ§e nesne oluÅŸturur --> AddScoped
+            /// <summary>
+ /// Understanding the life cycle of Dependency Injection (DI) is very important in ASP.Net Core applications. 
+ /// As we know, Dependency injection (DI) is a technique for achieving loose coupling between objects and their collaborators, or dependencies. 
+ /// Most often, classes will declare their dependencies via their constructor, allowing them to follow the Explicit Dependencies Principle. This approach is known as "constructor injection".
+ /// The lifetime of the service depends on when the dependency is instantiated and how long it lives.And lifetime depends on how we have registered those services.
+        /// The below three methods define the lifetime of the services,
+           //AddTransient
+           //Transient lifetime services are created each time they are requested.
+           //This lifetime works best for lightweight, stateless services.
+
+           //AddScoped
+           //Scoped lifetime services are created once per request.
+
+           //AddSingleton
+           //Singleton lifetime services are created the first time they are requested (or when ConfigureServices is run if you specify an instance there)
+           //and then every subsequent request will use the same instance.
+            /// </summary>
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
