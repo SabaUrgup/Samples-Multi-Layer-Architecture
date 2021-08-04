4b9fda0bc394348626d972fc9faa5cf950a62c31
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-08-04 10:58:37 +0300

    Swagger & controllers layer

---
+++ ManagementSolution/Management.WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using Management.Business.Abstract;
+using Management.DataAccessLayer.DataTransferObject.EmployeeDTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Management.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        [Route("EmployeeAdd")]
+        public ActionResult<string> AddEmployee(EmployeeAddDTO employee)
+        {
+            var result = _employeeService.AddEmployee(employee);
+
+            if (result > 0)
+            {
+                return Ok("İşçi ekleme başarılı!");
+            }
+            return BadRequest("İşçi ekleme başarısız!");
+        }
+
+        [HttpGet]
+        [Route("EmployeeList")]
+        public ActionResult<List<EmployeeListDTO>> ListEmployee()
+        {
+            var employeeList = _employeeService.ListEmployee();
+            if (employeeList == null)
+            {
+                return BadRequest("İşçi listesi bulunamadı!");
+            }
+            return Ok(employeeList);
+        }
+
+
+        [HttpPut]
+        [Route("EmployeeUpdate")]
+        public ActionResult<string> UpdateEmployee(EmployeeUpdateDTO employee)
+        {
+            var result = _employeeService.UpdateEmployee(employee);
+            if (result > 0)
+            {
+                return Ok("İşçi güncelleme başarılı!");
+            }
+            return BadRequest("İşçi güncelleme başarısız!");
+        }
+
+        [HttpDelete]
+        [Route("EmployeeDelete")]
+        public ActionResult<string> DeleteEmployee(int id)
+        {
+            var result = _employeeService.DeleteEmployee(id);
+            if (result > 0)
+            {
+                return Ok("İşçi silme başarılı!");
+            }
+            return BadRequest("İşçi silme başarısız!");
+        }
+
+    }
+}
---
+++ ManagementSolution/Management.WebAPI/Controllers/EmployerController.cs
@@ -0,0 +1,72 @@
+﻿using Management.Business.Abstract;
+using Management.DataAccessLayer.DataTransferObject.EmployerDTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Management.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployerController : ControllerBase
+    {
+        public IEmployerService _employerService;
+        public EmployerController(IEmployerService employerService)
+        {
+            _employerService = employerService;
+        }
+
+        [HttpPost]
+        [Route("EmployerAdd")]
+        public ActionResult<string> AddEmployer(EmployerAddDTO employer)
+        {
+            var result = _employerService.AddEmployer(employer);
+
+                if (result > 0)
+                {
+                    return Ok("İş veren ekleme başarılı!");
+                }
+                return BadRequest("İş veren ekleme başarısız!");
+        }
+
+        [HttpGet]
+        [Route("EmployerList")]
+        public ActionResult<List<EmployerListDTO>> ListEmployer()
+        {
+            var employerList = _employerService.ListEmployer();
+            if (employerList == null)
+            {
+                return BadRequest("İş veren listesi bulunamadı!");
+            }
+            return Ok(employerList);
+        }
+
+
+        [HttpPut]
+        [Route("EmployerUpdate")]
+        public ActionResult<string> UpdateEmployer(EmployerUpdateDTO employer)
+        {
+            var result = _employerService.UpdateEmployer(employer);
+                if (result > 0)
+            {
+                return Ok("İş veren güncelleme başarılı!");
+            }
+            return BadRequest("İş veren güncelleme başarısız!");
+        }
+
+        [HttpDelete]
+        [Route("EmployerDelete")]
+        public ActionResult<string> DeleteEmployer(int id)
+        {
+            var result = _employerService.DeleteEmployer(id);
+                if (result> 0)
+            {
+                return Ok("İş veren silme başarılı!");
+            }
+            return BadRequest("İş veren silme başarısız!");
+        }
+
+    }
+}
