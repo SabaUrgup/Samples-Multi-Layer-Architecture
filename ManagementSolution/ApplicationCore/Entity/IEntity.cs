1096a0e2ac4d035f923e65eaa14bded3e4438bef
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-08-04 09:44:03 +0300

    Common entities layer

---
+++ ManagementSolution/ApplicationCore/Entity/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entity
+{
+    /// <summary>
+    /// Varlıkların ortak ana özelliklerini barındıran interface
+    /// </summary>
+    public interface IEntity
+    { }
+}
---
+++ ManagementSolution/ApplicationCore/Entity/ISoftDelete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entity
+{
+    /// <summary>
+    /// Kontrollü silme işlemi yapan interface
+    /// </summary>
+    public interface ISoftDelete
+    {
+        /// <summary>
+        /// Silindi mi sorgusu yapan metot
+        /// </summary>
+        public bool IsDeleted { get; set; }
+    }
+}
